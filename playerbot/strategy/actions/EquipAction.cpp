0f407229e059c2cd0a9d76639ede7d0d3533440d
surray <surraymb@gmail.com>
2023-12-17 08:00:00 +0100

    implement bot auto equip fix

--- playerbot/strategy/actions/EquipAction.cpp
+++ playerbot/strategy/actions/EquipAction.cpp
@@ -189,7 +189,7 @@ bool EquipUpgradesAction::Execute(Event& event)
             return false;
         }
     }
-    else if (event.getSource() == "item push result" && sPlayerbotAIConfig.optimizeEquipUpgrading)
+    else if (event.getSource() == "item push result") // && sPlayerbotAIConfig.optimizeEquipUpgrading)
     {
         bool valid = false;
         WorldPacket& data = event.getPacket();
@@ -227,7 +227,7 @@ bool EquipUpgradesAction::Execute(Event& event)
                               itemProto->Class == ItemClass::ITEM_CLASS_ARMOR ||
                               itemProto->Class == ItemClass::ITEM_CLASS_CONTAINER))
             {
-                return false;
+                valid = true;
             }
         }
 
